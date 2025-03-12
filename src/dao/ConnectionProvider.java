56ce6f110c4bf16b0e106a49d5e832564af98927
brayandavidco <brayandavidco@gmail.com>
2025-03-12 17:59:28 -0500

    adelanto casa

---
+++ src/dao/ConnectionProvider.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author Brayan
+ */
+public class ConnectionProvider {
+    
+    public static Connection getCon(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/inventory?useSSL=false","root","Incorrecta"); //Incorrecta=contraseña base de datos, no error :P
+            return con;
+        }
+        catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
+        
+        
+    }
+    
+}
+
+
+
+
+
+
+
---
+++ src/dao/tables.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Brayan
+ */
+public class tables {
+
+    public static void main(String[] args) {
+        Connection con = null;
+        Statement st = null;
+        try {
+            con = ConnectionProvider.getCon();
+            st = con.createStatement();
+            //           st.executeUpdate("create table appuser(appuser_pk int AUTO_INCREMENT primary key, userRole varchar(50), name varchar(200),mobileNumber varchar(50),email varchar(200),password varchar(50),address varchar(200), status varchar(50))");
+            st.executeUpdate("insert into appuser(userRole,name,mobileNumber,email,password,address,status) values('SuperAdmin','Super Admin','3178682331','prubaSadmin@testgmail.com','0312','calle 23b #23-33','Active')");
+            JOptionPane.showMessageDialog(null, "tabla creada correctamente");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        } finally {
+            try {
+                con.close();
+                st.close();
+            } catch (Exception e) {
+            }
+        }
+
+    }
+
+}
